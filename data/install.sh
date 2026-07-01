14f2fae8bc07f179b889c1c2efaaa23a0db73861
James Hane <James.Hane@curtin.edu.au>
2026-07-01 22:20:47 +0800

    Delete data/install.sh

--- data/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Downloading Remeff databases from Figshare..."
-FIGSHARE_URL="https://figshare.com/ndownloader/files/66183590" #from https://doi.org/10.6084/m9.figshare.32859416
-wget -O SUSSdata.tar.gz "$FIGSHARE_URL"
-echo "Extracting databases..."
-tar -xzf SUSSdata.tar.gz
-rm SUSSdata.tar.gz
-echo "Installation complete!"
